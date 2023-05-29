7e074c6bc9f04670f1764de1e1c40e864335fe2a
Igor <gogakorovin@yandex.ru>
2023-05-29 20:18:54 +0500

    Taski workflow and tests

--- backend/api/tests.py
+++ backend/api/tests.py
@@ -1,5 +1,6 @@
 from http import HTTPStatus
 from api import models
+
 from django.test import Client, TestCase
 
 
@@ -18,4 +19,3 @@ class TaskiAPITestCase(TestCase):
         response = self.guest_client.post('/api/tasks/', data=data)
         self.assertEqual(response.status_code, HTTPStatus.CREATED)
         self.assertTrue(models.Task.objects.filter(title='Test').exists())
-
